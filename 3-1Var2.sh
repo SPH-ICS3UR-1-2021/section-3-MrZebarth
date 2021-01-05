05eab155dc16b94bcc9d9de1a0a09de85795b737
Kris Zebarth <kris.zebarth@ocsb.ca>
2021-01-05 16:39:51 +0000

    Added run commands for replit

---
+++ 3-1Var2.sh
@@ -0,0 +1,2 @@
+javac -d classes -cp classes 3-1/Variables02/src/variables02/*.java
+java -cp classes variables02/Variables02 
---
+++ 3-2.sh
@@ -0,0 +1,2 @@
+javac -d classes -cp classes 3-2/Chickens01/src/chickens01/*.java
+java -cp classes chickens01/Chickens01
